d9500de37ec15585cff7eadf6456fa800fd97306
alandeesina <53952564+alandeesina@users.noreply.github.com>
2019-09-23 13:56:32 +0200

    HermitConfiguration.java
    test for simulation, nothing changed except adding a * symbol

--- simulation/src/com/agentecon/exercises/HermitConfiguration.java
+++ simulation/src/com/agentecon/exercises/HermitConfiguration.java
@@ -3,6 +3,7 @@
  * Copyright: Meisser Economics AG, Zurich
  * Contact: luzius@meissereconomics.com
  *
+ *
  * Feel free to reuse this code under the MIT License
  * https://opensource.org/licenses/MIT
  */
